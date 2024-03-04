91bfa4a0ba04adcb7a85ab92aaaf8cc04e474674
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-03-04 17:18:26 +0100

    game proceeds to the next level upon reaching exit

--- tutorial_2/characters/collectible.gd
+++ tutorial_2/characters/collectible.gd
@@ -6,5 +6,8 @@ extends Area2D
 func _on_body_entered(body):
 	if (body.name == "main_character"):
 		sprite.animation = "collected"
-		queue_free()
 		game_manager.add_point()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	queue_free()
